51f7bc969461136aa6bfc5d52fd2eef2c67b8ecd
Nathalia Fernandes <nathyfasantos@gmail.com>
2025-01-27 12:23:15 -0300

    Desafio Megasena - Resposta #01

---
+++ src/components/mega/sorteio.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function gerarNumeros(qtde){
+    const numeros = Array(qtde)
+        .fill(0)
+        .reduce((nums) => {
+            const novoNumero = gerarNumeroNaoContido(1, 60, nums)
+            return [...nums, novoNumero]
+        }, [])
+        .sort((n1, n2) => n1 - n2)
+    return numeros
+}
+
+function gerarNumeroNaoContido(min, max, array){
+    const aleatorio = parseInt(Math.random() * (max + 1 - min)) + min
+    return array.includes(aleatorio) ? 
+                gerarNumeroNaoContido(min, max, array) : aleatorio
+}
